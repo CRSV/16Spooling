467e98982cb1696b5c80de1dc8955375a30e9e84
guillaumelgauthier <39998602+guillaumelgauthier@users.noreply.github.com>
2018-08-29 11:49:10 -0400

    Update poolscreation.R

--- poolscreation.R
+++ poolscreation.R
@@ -1,7 +1,9 @@
-Samplelist <-read.table(file="listetruies.txt") #Load the list of samples
-X<-X #X samples per pool
-Y<-Y #Y pools to create  
-Pools<-as.data.frame(matrix(,X,Y))
+Samplelist <-read.table(file="listeoeufs.txt") #Load the list of samples
+
+Z<-10 #X Maximum samples per pool
+Y<-44 #Y pools to create  
+for(X in 2:Z) #Repeat to the maximum pool size
+{Pools<-as.data.frame(matrix(,X,Y))
 for (i in 1:Y)#Repeat for the number of pools to create
 {individuals<-sample.int(Y,X)  #Randomly draw X samples Y times    
 Pools[,i]<- Samplelist[individuals,]} #Replace random numbers with samples names
@@ -10,4 +12,5 @@ Pools[,2] <- rep("/",nrow(Pools)) #Add / at the end of each row
 Pools$reps<-rep(1:Y, each=X) # Add to wich pools each sample is part of
 Pools<-Pools[c(3,1,2)]
 write.table(Pools,file= paste("Poolslist",X,".txt", sep=""), quote = FALSE, row.names = FALSE, col.names = FALSE)#Export to a text file
+}
 
